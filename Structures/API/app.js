63a43662931d8bea19258570790b555e997e34f6
The-Repo-Club <wayne6324@gmail.com>
2022-03-04 14:44:11 +0000

    Added ratelimit

--- Structures/API/app.js
+++ Structures/API/app.js
@@ -3,10 +3,19 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const rateLimit = require("express-rate-limit");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var levelsRouter = require("./routes/levels");
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 25, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	message: "Too many requests from this IP, please try again after 15 minutes",
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 var app = express();
 const port = 3000;
@@ -21,9 +30,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/levels", levelsRouter);
-app.use("/users", usersRouter);
+app.use("/", indexRouter, limiter);
+app.use("/levels", levelsRouter, limiter);
+app.use("/users", usersRouter, limiter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
