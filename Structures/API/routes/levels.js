b1f584b68b85ca37855da7096c55eb94ce59ca2f
The-Repo-Club <wayne6324@gmail.com>
2022-03-04 14:12:23 +0000

    format api as json

--- Structures/API/routes/levels.js
+++ Structures/API/routes/levels.js
@@ -2,9 +2,15 @@ var express = require("express");
 var router = express.Router();
 const Levels = require("../../../Systems/levelsSys");
 
+router.get("/", async function (req, res, next) {
+	if (!req.params.guildID)
+		return res.send({ error: ["No guildID was passed"] });
+});
+
 /* GET users listing. */
 router.get("/:guildID", async function (req, res, next) {
-	if (!req.params.guildID) return res.send("No guildID was sent.");
+	if (!req.params.guildID)
+		return res.send({ error: ["No guildID was passed"] });
 	const LeaderBoard = await Levels.fetchLeaderboard(req.params.guildID, 1000);
 
 	res.send(LeaderBoard);
--- Structures/API/routes/users.js
+++ Structures/API/routes/users.js
@@ -3,9 +3,15 @@ var router = express.Router();
 
 const client = require("../../index");
 
+router.get("/", async function (req, res, next) {
+	if (!req.params.guildID)
+		return res.send({ error: ["No guildID was passed"] });
+});
+
 /* GET users listing. */
 router.get("/:guildID", function (req, res, next) {
-	if (!req.params.guildID) return res.send("No guildID was sent.");
+	if (!req.params.guildID)
+		return res.send({ error: ["No guildID was passed"] });
 	const list = client.guilds.cache.get(req.params.guildID);
 
 	res.send(list.members.cache);
